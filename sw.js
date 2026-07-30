e93c99537bf751ef489125847f3822185ce391cb
davross99 <davross99@hotmail.com>
2026-07-30 04:36:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener('fetch', event => {
-event.respondWith(fetch(event.request));
-}); 
+const CACHE_NAME = 'jack-game-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
