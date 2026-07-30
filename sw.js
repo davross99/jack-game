9f34ee8629852655a4cbd988c9cdc311f0bdd9ec
davross99 <davross99@hotmail.com>
2026-07-30 10:38:31 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,13 +10,26 @@ self.addEventListener('install', (event) => {
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request).catch(() => {
-        return caches.match('./index.html');
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).catch(() => {
+        return caches.match('./index.html').then((fallback) => {
+          return fallback || new Response(
+            '<h1>Offline</h1>',
+            { headers: { 'Content-Type': 'text/html' } }
+          );
+        });
       });
     })
   );
